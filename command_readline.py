b983614eea834d34f46bd39cc4b1cfc84eaaff82
zxn <123@qq.com>
2017-06-30 15:34:07 +0800

    test

---
+++ command_readline.py
@@ -0,0 +1,18 @@
+import sys
+
+def main():
+	while 1:
+		msg = sys.stdin.readline()
+		print msg
+
+
+
+
+if __name__ == '__main__':
+	if(len(sys.argv) < 3):
+		print "ghgfhfdfdgdgfdhdf"
+		sys.exit()
+	print sys.argv[0]
+
+
+	main()
